27db528fa26d36d40e4b1feecdb8ac7311a53d3a
Olga Bogdanova <TomieForever@gmail.com>
2022-01-18 13:25:42 +0300

    feat: add html filters

--- my-monster-dolls/src/app/components/adv-search-page/adv-search-page.component.ts
+++ my-monster-dolls/src/app/components/adv-search-page/adv-search-page.component.ts
@@ -1,23 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { lastValueFrom } from 'rxjs';
+import { IDoll } from '../../models';
+import { DollsService } from '../../services/dolls-service.service';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-adv-search-page',
   templateUrl: './adv-search-page.component.html'
 })
 export class AdvSearchPageComponent implements OnInit {
+  public dolls!: IDoll[];
+  public lastPage = 1;
 
-  constructor(private router: Router) { }
+  constructor(private dollsService: DollsService, private router: Router) { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    this.dolls = await lastValueFrom(this.dollsService.getDollsPage(this.lastPage));
   }
-  title = 'routing-app';
 
   public goToSearchPage() {
     this.router.navigate(['/search-component']);
   }
-  public goToAdvSearchPage() {
-    this.router.navigate(['/adv-search-component']);
+
+   public async onLoadMore() {
+    const newDolls = await lastValueFrom(this.dollsService.getDollsPage(this.lastPage + 1));
+
+    this.dolls = [...this.dolls, ...newDolls];
+    this.lastPage += 1;
   }
 
 
